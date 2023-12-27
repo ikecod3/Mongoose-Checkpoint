1ee18a70deee7d110d0ab4bbd3363394620c7f2e
IKE COD3 <ikechukwuokoye15@gmail.com>
2023-12-27 01:34:15 +0100

    first commit

--- 07_findOneAndUpdate.js
+++ 07_findOneAndUpdate.js
@@ -1,12 +1,12 @@
 import Person from "./models/Person.js";
 
 // Function to find a person by Name, set their age to 20, and return the updated document
-const findAndUpdatePersonAgeAndSetName = async (personName) => {
+const findAndUpdatePersonAgeAndSetName = async (personName, age) => {
   try {
     // Using Model.findOneAndUpdate() to find a person by name and update age
     const updatedPerson = await Person.findOneAndUpdate(
       { name: personName }, // Search key
-      { $set: { age: 20 } }, // Update operation
+      { $set: { age: age } }, // Update operation
       { new: true } // Options to return the updated document
     );
     // Check if a person was found and updated
